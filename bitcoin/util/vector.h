0ab756bdd9ae6632e60273f96e70b533ffb2f0ca
Pieter Wuille <pieter.wuille@gmail.com>
2019-08-28 15:49:42 -0700

    Better Vector function

--- bitcoin/util/vector.h
+++ bitcoin/util/vector.h
@@ -6,47 +6,31 @@
 #define _BITCOIN_UTIL_VECTOR_H_ 1
 
 #include <vector>
-#include <utility>
+#include <type_traits>
 
-namespace vector {
-namespace internal {
-
-/** Compute the number of arguments passed. */
-inline constexpr unsigned NumArgs() { return 0; }
-template<typename Arg, typename... Args>
-inline constexpr unsigned NumArgs(Arg&&, Args&&... args) { return 1 + NumArgs(std::forward<Args>(args)...); }
-
-/** Push back multiple elements to a container. */
+/** Emplace back multiple elements to a container. */
 template<typename V>
-inline void PushBackMany(V& vec) {}
+inline void EmplaceBackMany(V& vec) {}
 template<typename V, typename Arg, typename... Args>
-inline void PushBackMany(V& vec, Arg&& arg, Args&&... args)
+inline void EmplaceBackMany(V& vec, Arg&& arg, Args&&... args)
 {
-    vec.push_back(std::forward<Arg>(arg));
-    PushBackMany(vec, std::forward<Args>(args)...);
+    vec.emplace_back(std::forward<Arg>(arg));
+    EmplaceBackMany(vec, std::forward<Args>(args)...);
 }
-
-} // namespace internal
-} // namespace vector
 
 /** Construct a vector with the specified elements.
  *
  * This is preferable over the list initializing constructor of std::vector:
- * - It automatically infers the element type of the vector.
- * - If arguments are rvalue references, they will be moved into the vector
+ * - It automatically infers the element type from its arguments.
+ * - If any arguments are rvalue references, they will be moved into the vector
  *   (list initialization always copies).
- *
- * The first argument is used to determine the vector's element type. The
- * other arguments must be convertible to that type.
  */
-template<typename Arg, typename... Args>
-inline std::vector<typename std::remove_cv<typename std::remove_reference<Arg>::type>::type> Vector(Arg&& arg, Args&&... args)
+template<typename... Args>
+inline std::vector<typename std::common_type<Args...>::type> Vector(Args&&... args)
 {
-    using namespace vector::internal;
-    std::vector<typename std::remove_cv<typename std::remove_reference<Arg>::type>::type> ret;
-    ret.reserve(1 + NumArgs(std::forward<Args>(args)...));
-    ret.push_back(std::forward<Arg>(arg));
-    PushBackMany(ret, std::forward<Args>(args)...);
+    std::vector<typename std::common_type<Args...>::type> ret;
+    ret.reserve(sizeof...(args));
+    EmplaceBackMany(ret, std::forward<Args>(args)...);
     return ret;
 }
 
